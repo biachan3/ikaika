a0e54d8b8fb6271df2850555a6f9a442c16e8f35
GWOUBAYA <s160419048@student.ubaya.ac.id>
2023-03-09 22:35:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CustomerTicketController;
 
 /*
 |--------------------------------------------------------------------------
