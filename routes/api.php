7f35028864dce51d686a8ce8453d7efdda5afc1b
ferdiannovendra <ferdiannovendra15@gmail.com>
2023-03-12 18:20:34 +0700

    temp

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('api/payment')->controller(PaymentController::class)->group(
+    function () {
+        Route::post('/notification/handling', 'notifHandling')->name("notifHandling");
+    }
+);
