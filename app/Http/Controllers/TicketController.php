f775ae6b5898dbcb5ae56257f89b2269467a58eb
ferdiannovendra <ferdiannovendra15@gmail.com>
2023-05-03 16:48:58 +0700

    handle id not found

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -182,13 +182,20 @@ class TicketController extends Controller
         $detail_tx = Ticket::find($id);
         // dd($detail_tx);
         $qrcode="";
-        if($detail_tx->transaction_status =="Sukses" || $detail_tx->transaction_status =="settlement")
-        {
-            $qrcode = base64_encode(QrCode::format('svg')->size(150)->errorCorrection('H')->generate(url($detail_tx->id)));
-            $qrcode = QrCode::generate($detail_tx->id);
+        if ($detail_tx != null) {
+            if($detail_tx->transaction_status =="Sukses" || $detail_tx->transaction_status =="settlement")
+            {
+                $qrcode = base64_encode(QrCode::format('svg')->size(150)->errorCorrection('H')->generate(url($detail_tx->id)));
+                $qrcode = QrCode::generate($detail_tx->id);
 
+            }
+            return view('user.ticket.orderDetail', compact('detail_tx','qrcode'));
+        } else {
+            return view('user.ticket.orderDetail404', compact('id'));
+            # code...
         }
-        return view('user.ticket.orderDetail', compact('detail_tx','qrcode'));
+
+
     }
     /**
      * Display the specified resource.
