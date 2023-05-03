0b7655493a0cfdf40705e61ca6edfd3b60943201
ferdiannovendra <ferdiannovendra15@gmail.com>
2023-05-03 17:08:06 +0700

    subject nama dan body

--- app/Mail/InfoRegistrationMail.php
+++ app/Mail/InfoRegistrationMail.php
@@ -32,7 +32,7 @@ class InfoRegistrationMail extends Mailable
     {
         $nama = $this->details['nama'];
         // dd($nama);
-        return $this->subject("Informasi Pendaftaran Reuni 55 IKA Ubaya $nama")
+        return $this->subject("Informasi Pendaftaran Reuni 55 IKA Ubaya - $nama")
                     ->view('emails.infoemail');
     }
 }
