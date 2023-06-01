087d5b1d0acfdd0c96c9dce52e0b6ae3bd249e7f
biachan3 <korekifaa123@gmail.com>
2023-06-01 14:17:12 +0700

    asdasdas

--- resources/views/admin/sidebar/add-data-manual.blade.php
+++ resources/views/admin/sidebar/add-data-manual.blade.php
@@ -79,14 +79,11 @@
                 <span>Data Kehadiran</span>
             </a>
         </li>
-        <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}
-            </a>
+        <li class="nav-item">
 
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
+
+            <div class="">
+                <a class="" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
